86e3ffa496731a1fe22282e1b0b13af01cc76ed2
r-connors <programmer@hs-lab-9.local>
2015-10-23 08:58:15 -0400

    random answer test

--- Meme.Machine.py
+++ Meme.Machine.py
@@ -1,2 +1,8 @@
-# Hello world
-# Hello again
+import random
+
+def test_random():
+    print (["1","2","3"])
+    print random.choice(["regular pepe","rare pepe","legendary pepe"])
+
+test_random()
+    
