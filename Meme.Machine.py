e0e25e0713254d2f0049decc65f1a9fa765e36ed
r-connors <programmer@hs-lab-9.local>
2015-10-26 13:16:45 -0400

    Robert&Geroge get multi test(Worked on together)

--- Meme.Machine.py
+++ Meme.Machine.py
@@ -6,4 +6,13 @@ def test_random():
     print random.choice(["regular pepe","rare pepe","legendary pepe"])
 
 test_random()
+
+
+
+def test_getmulti():
+    raw_input(["chocolate","vanilla","strawberry"])
+    print (["Chocolate","Vanilla","Strawberry"])
+
+test_getmulti
+    
     
